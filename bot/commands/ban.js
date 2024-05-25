ea083861f2a8da1bc35f48710cfd92c9ab76d77e
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 17:24:47 +0530

    Add bot/commands/ban.js

---
+++ bot/commands/ban.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'ban',
+  description: 'Bans a member from the server',
+  execute(message, args) {
+    // Ban logic here
+  }
+}
