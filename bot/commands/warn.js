b6749f55ce7e5dcae88189d572b0eb757cca2265
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 17:24:48 +0530

    Add bot/commands/warn.js

---
+++ bot/commands/warn.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'warn',
+  description: 'Warns a member in the server',
+  execute(message, args) {
+    // Warn logic here
+  }
+}
