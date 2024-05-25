dea568a1677edda46912934f5dbed5448235e407
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 17:24:47 +0530

    Add bot/commands/mute.js

---
+++ bot/commands/mute.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'mute',
+  description: 'Mutes a member in the server',
+  execute(message, args) {
+    // Mute logic here
+  }
+}
