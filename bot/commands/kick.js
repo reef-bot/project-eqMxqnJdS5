fe85b60b28d6a1bdeb4ec6d9ab36c21cbd9481ff
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 17:24:46 +0530

    Add bot/commands/kick.js

---
+++ bot/commands/kick.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'kick',
+  description: 'Kicks a member from the server',
+  execute(message, args) {
+    // Kick logic here
+  }
+}
