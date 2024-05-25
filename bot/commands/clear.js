cf3fc6386dfbbca84982caf80d1d3852df2b31f6
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 17:24:48 +0530

    Add bot/commands/clear.js

---
+++ bot/commands/clear.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'clear',
+  description: 'Clears messages in the chat',
+  execute(message, args) {
+    // Clear chat logic here
+  }
+}
